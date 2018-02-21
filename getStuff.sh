e4753ac8aab1ba16f1ca96b9023687b61ccee43a
Travis <immyouany@gmail.com>
2018-02-20 17:29:06 -0800

    Adding logic for auth key

--- getStuff.sh
+++ getStuff.sh
@@ -11,32 +11,27 @@ read accessCode
 
 #specify the file type
 echo "Thanks! Your files will be downloaded to ~/Downloads/"$subdomain
-echo "Last Question! What filetype do you want to download? You can say: audio, document, photo, video, webapp. You can specify multiple types as well"
-read filetype
 
 #silly banter
-echo "commencing download in..."
-sleep 1
-echo 3
-sleep 1
-echo 2
-sleep 1
-echo 1
-sleep 1
-echo "...begin!"
+echo "Let's Go!"
 sleep 1
 
 #API requests:
-getAssetIds=( $(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.showpad.biz/api/v3/assets.json?filetype='"$filetype"'&fields=id&limit=1000' | jq .response.items[].id | tr -d \") )
+getAssetIds=( $(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.showpad.biz/api/v3/assets.json?fields=id&isDownloadable=true&limit=1000' | jq .response.items[].id | tr -d \") )
 
 for id in ${getAssetIds[@]}
 do
 	downloadLinks=( $(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.showpad.biz/api/v3/assets/'"${id}"'.json?fields=shortLivedDownloadLink,name' | jq -r .response.shortLivedDownloadLink) )
-#Download
+	response=$(curl -s -o /dev/null -w '%{http_code}\n' $downloadLinks)
+if (($response == 401))
+then
+    echo "Please enter a new Authorization Key"
+		read accessCode
+else
 	wget --content-disposition ${downloadLinks[@]} -P ~/Downloads/$subdomain
-
+fi
 done
  $ wget 2> error.log
 
- echo "The script took " 
+ echo "The script took "
  time
