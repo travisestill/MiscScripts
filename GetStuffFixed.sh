e92001f7946cb31dcf26e6074e1a45fdf5974648
TravisE <6887913+travisestill@users.noreply.github.com>
2021-08-25 15:20:53 -0700

    Update GetStuffFixed.sh

--- GetStuffFixed.sh
+++ GetStuffFixed.sh
@@ -2,11 +2,11 @@
 
 #prompts
 #specify subdomaind
-echo "Which showpad instance would you like to download from? Please type the subdomain ONLY:"
+echo "Which instance would you like to download from? Please type the subdomain ONLY:"
 read subdomain
 
 #specify OAuth access token
-echo "Thanks! Now go to the API Explorer in the target Showpad account and copy the Access Code. Paste it below:"
+echo "Thanks! Now go to the API Explorer in the target account and copy the Access Code. Paste it below:"
 read accessCode
 
 #specify the file type
@@ -27,11 +27,11 @@ echo "...begin!"
 sleep 1
 
 #API requests:
-getAssetIds=( $(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.showpad.biz/api/v3/assets.json?filetype='"$filetype"'&fields=id&limit=1000' | jq .response.items[].id | tr -d \") )
+getAssetIds=( $(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.domain.biz/api/v3/assets.json?filetype='"$filetype"'&fields=id&limit=1000' | jq .response.items[].id | tr -d \") )
 
 for id in ${getAssetIds[@]}
 do
-	getNames=( $(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.showpad.biz/api/v3/assets/'"${id}"'?fields=name' | jq .response.name | tr -d \" ) )
+	getNames=( $(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.domain.biz/api/v3/assets/'"${id}"'?fields=name' | jq .response.name | tr -d \" ) )
 	echo $getNames
 #	download=($(curl -X GET --header 'Accept: application/json' --header 'Authorization: Bearer '"$accessCode"'' 'https://'"$subdomain"'.showpad.biz/api/v3/assets/'"${id}"'/file/v1?filename='"$getNames"'' -O -J) )
 done
